54feb77eb02465c7f1cd72b3e56081f8c4f06b83
Maria2020-Sa <maugesara@gmail.com>
2024-06-09 17:53:29 -0300

    adding list reading function

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,3 +1,6 @@
+#Crear un programa que almacene en una lista las materias de esta u otra carrera y que las
+#muestre por pantalla. (La lista debe ser predefinida, no debe ser ingresada por el usuario).
+
 def materias_clase():
     lista_materias=["programaciónI", "Introducción a la Informatica","Diseño Gráfico", "Arquitectura de Computadoras", "Programación II"]
     print(f"Materias de Programación Universitaria en Desarrollo Web 1er año: \n{lista_materias}")
