4e044c93c496c2c762bff42975b70758d6bdb8f1
Maria2020-Sa <maugesara@gmail.com>
2024-06-09 17:03:43 -0300

    adding list reading function

---
+++ ejercicio_1.py
@@ -0,0 +1,5 @@
+def materias_clase():
+    lista_materias=["programaciónI", "Introducción a la Informatica","Diseño Gráfico", "Arquitectura de Computadoras", "Programación II"]
+    print(f"Materias de Programación Universitaria en Desarrollo Web 1er año: \n{lista_materias}")
+
+materias_clase()
